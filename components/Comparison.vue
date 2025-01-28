a5a5a67c60dca25bfa254c59ae9a9e6d32b69a4a
harodler <haj.rodler@gmail.com>
2025-01-28 22:47:52 +0100

    chore: dynamic article context loading

--- components/Comparison.vue
+++ components/Comparison.vue
@@ -1,27 +1,12 @@
 <template>
-  <div class="">
-    <h2>Kontext Artikel 1</h2>
+  <div class="section">
+    <h2>Kontext Artikel {1}</h2>
     <div class="flex flex-col justify-center items-center w-full px-5 md:px-10">
       <!-- Kontext Section -->
       <div class="mb-b">
         <div>
-          <h3>Viele Tote bei israelischen Angriffen in Gaza gemeldet</h3>
-          <p>
-            Die israelische Armee hat bei mehreren Angriffen im Gazastreifen
-            einem Bericht der palästinensischen Nachrichtenagentur WAFA zufolge
-            mindestens 41 Menschen getötet. Zahlreiche weitere Menschen seien
-            bei den Angriffen in Dschabalija im Norden sowie Deir al-Balah im
-            Zentrum und Rafah im Süden des Küstenstreifens verletzt worden,
-            schrieb die Agentur unter Berufung auf Angaben von gestern aus
-            medizinischen Kreisen in dem Küstenstreifen.
-          </p>
-          <p>
-            Von israelischer Seite gab es bisher keine Angaben zum
-            Kampfgeschehen oder zu dem Bericht. Später gab das Militär bekannt,
-            in Deir al-Balah mit einem Präzisionsschlag vier bewaffnete
-            Hamas-Kämpfer in einem Fahrzeug getroffen zu haben. Die Angaben
-            beider Seiten ließen sich nicht unabhängig verifizieren.
-          </p>
+          <h3>{{ article.context.title }}</h3>
+          {{ article.context.body }}
         </div>
       </div>
     </div>
@@ -35,7 +20,7 @@
       verySoftText="Die israelische Armee hat bei mehreren Aktionen im Gazastreifen einem Bericht der palästinensischen Nachrichtenagentur WAFA zufolge eine hohe Anzahl von Opfern verursacht."
     ></Paraphrases>
     <!-- <div class="divide-y"></div> -->
-    <Paraphrases
+    <!-- <Paraphrases
       articleId="story_x"
       softerPromptNum="5"
       verySoftPromptNum="7"
@@ -52,11 +37,38 @@
       originalText="Von israelischer Seite gab es bisher keine Angaben zum Kampfgeschehen oder zu dem Bericht. "
       softerText="Von israelischer Seite gab es bisher keine offiziellen Angaben zu den Vorfällen."
       verySoftText="Von israelischer Seite gab es bisher keine Angaben zu den Vorgängen oder zu dem Bericht."
-    ></Paraphrases>
+    ></Paraphrases> -->
+    <!-- <div> -->
+    <!-- Loop through each sentence -->
+    <!-- <div v-for="(sentence, sentenceKey) in sentences" :key="sentenceKey"> -->
+    <!-- Loop through "softer" versions of the sentence -->
+    <!-- <div
+          v-for="(softerVersion, index) in sentence.versions.softer"
+          :key="'softer-' + index"
+        >
+          <Paraphrases
+            :softerPromptNum="softerVersion.prompt_id"
+            :verySoftPromptNum="sentence.versions.verySoft[index]?.prompt_id"
+            :sentence="sentenceKey"
+            :originalText="sentence.original"
+            :softerText="softerVersion.sentence"
+            :verySoftText="sentence.versions.verySoft[index]?.sentence"
+          />
+        </div>
+      </div> -->
+    <!-- </div> -->
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const props = defineProps({
+  article: {
+    type: Object,
+    required: true,
+  },
+});
+console.log("Comparison");
+</script>
 
 <style scoped>
 /* You can add any additional styles here if needed */
